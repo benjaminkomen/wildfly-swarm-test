5105942cc3a4d2cd69f57471bcf82e5d9cc168f2
Benjamin Komen <komenb@gmail.com>
2018-08-19 20:19:14 +0200

    enabling swagger (partly done)

--- src/main/java/com/benjamin/serviceinterface/HelloResource.java
+++ src/main/java/com/benjamin/serviceinterface/HelloResource.java
@@ -1,15 +1,26 @@
 package com.benjamin.serviceinterface;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-//@Path("/")
-//public class HelloResource {
-//
-//    @GET
-//    @Path("/hello")
-//    public Response getHello() {
-//        return Response.ok("hi benji").build();
-//    }
-//}
+@Path("/")
+@Api(value = "Hello")
+public class HelloResource {
+
+    @GET
+    @Path("/hello")
+    @Produces(MediaType.TEXT_PLAIN)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "A simple answer to confirm everything is working")
+    })
+    public Response getHello() {
+        return Response.ok("hello to the w0rld").build();
+    }
+}
